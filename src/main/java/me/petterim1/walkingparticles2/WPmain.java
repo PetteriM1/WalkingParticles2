704c557a798feb38ad7384470d39db0e53f8f129
Petteri <26197131+PetteriM1@users.noreply.github.com>
2021-11-10 09:24:18 +0200

    Initial commit

---
+++ src/main/java/me/petterim1/walkingparticles2/WPmain.java
@@ -0,0 +1,53 @@
+package me.petterim1.walkingparticles2;
+
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WPmain extends PluginBase {
+
+    private Config data;
+
+    static Map<String, String> effects;
+
+    static List<String> bannedEffects;
+
+    public void onLoad() {
+        if (getDescription().getAuthors().size() != 1 || !getDescription().getAuthors().get(0).equals("PetteriM1") || !getDescription().getVersion().startsWith("2") || !getDescription().getName().equals("WalkingParticles") || !getDescription().getDescription().equals("Walking particles plugin for Nukkit")) {
+            System.exit(1);
+        }
+    }
+
+    public void onEnable() {
+        if (getDescription().getAuthors().size() != 1 || !getDescription().getAuthors().get(0).equals("PetteriM1") || !getDescription().getVersion().startsWith("2") || !getDescription().getName().equals("WalkingParticles") || !getDescription().getDescription().equals("Walking particles plugin for Nukkit")) {
+            System.exit(1);
+            return;
+        }
+        saveDefaultConfig();
+        saveResource("data.yml");
+        data = new Config(getDataFolder() + "/data.yml", Config.YAML);
+        effects = (Map<String, String>) data.get("effects");
+        if (null == effects) {
+            effects = new HashMap<>();
+            getLogger().info("No data found");
+        }
+        bannedEffects = getConfig().getStringList("bannedEffects");
+        if (null == bannedEffects) {
+            bannedEffects = new ArrayList<>();
+            getLogger().info("Banned effects list was null, replaced wth an empty list");
+        }
+        getServer().getCommandMap().register("walkingparticles", new WPcommand());
+        getServer().getPluginManager().registerEvents(new WPevents(), this);
+        getLogger().info("§cWalkingParticles §aby PetteriM1 enabled!");
+        getLogger().info("§6Please consider leaving a rating on nukkitx.com if you like the plugin :)");
+    }
+
+    public void onDisable() {
+        data.set("effects", effects);
+        data.save(false);
+    }
+}
